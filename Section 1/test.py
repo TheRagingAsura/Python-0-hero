ae3ddaf33d0d335a0736b9b1153052d5b2d9fb42
TheRagingAsura <89212217+TheRagingAsura@users.noreply.github.com>
2022-11-19 22:08:55 -0500

    2

--- Section 1/test.py
+++ Section 1/test.py
@@ -1,3 +1,8 @@
-f.write("\n Appeneded to the end of my text file?")
-f = open("Test.txt","w")
-f.close()
+mylist = [1,2,3,4,5,6,7,8,9,10]
+
+for numbah in mylist:
+    #Check for even
+    if numbah % 2 == 0:
+        print(numbah)
+    else:
+        print(f'Odd number: {numbah}')
